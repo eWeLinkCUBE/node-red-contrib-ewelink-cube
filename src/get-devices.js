84427d255b1c6fc44c7098cad46eb2c8b40d7eff
liyeping <1498328576@qq.com>
2023-02-10 10:28:22 +0800

    Initialize template

--- src/get-devices.js
+++ src/get-devices.js
@@ -1,6 +1,11 @@
 module.exports = function (RED) {
     function GetDevicesNode(config) {
         RED.nodes.createNode(this, config);
+
+        this.on('input', async (msg) => {
+            msg.payload='test';
+            this.send(msg);
+        });
     }
 
     RED.nodes.registerType('get-devices', GetDevicesNode);
