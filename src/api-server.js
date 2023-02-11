3c2b15597aa899036d2805f4faf7d43f14b99081
Tony Yuan <tony.yuan@coolkit.cn>
2023-02-11 16:13:04 +0800

    Add `get-bridge-info`, `get-bridge-token` and `get-device-list` API

--- src/api-server.js
+++ src/api-server.js
@@ -1,12 +1,49 @@
+const ApiClient = require('./extern/libapi').default.ihostApi;
+
 module.exports = function (RED) {
+    let apiClient = null;
+
     function ApiServerNode(config) {
         RED.nodes.createNode(this, config);
     }
 
-    RED.httpAdmin.post('/hello', (req, res) => {
-        // req.body
-        const data = 'world';
-        res.send(data);
+    RED.httpAdmin.post('/get-bridge-info', (req, res) => {
+        const ip = req.body.ip;
+
+        apiClient = new ApiClient({ ip });
+        apiClient.getBridgeInfo()
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                // TODO: handle err
+                res.send(JSON.stringify({ error: 500, msg: 'getBridgeInfo() error' }));
+            });
+    });
+
+    RED.httpAdmin.post('/get-bridge-token', (req, res) => {
+        const ip = req.body.ip;
+
+        apiClient = new ApiClient({ ip });
+        apiClient.getBridgeAT({})
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                // TODO: handle err
+                res.send(JSON.stringify({ error: 500, msg: 'getBridgeAT() error' }));
+            })
+    });
+
+    RED.httpAdmin.post('/get-device-list', (req, res) => {
+        apiClient.getDeviceList()
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                // TODO: handle err
+                res.send(JSON.stringify({ error: 500, msg: 'getDeviceList() error' }))
+            });
     });
 
     RED.nodes.registerType('api-server', ApiServerNode);
