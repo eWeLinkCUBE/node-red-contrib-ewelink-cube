d6165b2d0578951a4d63fb38e3c2e1079cd5ad57
Tony Yuan <tony.yuan@coolkit.cn>
2023-02-11 09:28:57 +0800

    Add extern libapi

---
+++ src/extern/libapi/api/ihostClass.d.ts
@@ -0,0 +1,11 @@
+import baseClass from "./baseClass";
+export default class ihostClass extends baseClass {
+    constructor(config: {
+        ip: string;
+        debug?: boolean;
+    });
+    /**
+     * 获取网关运行状态
+     */
+    getBridgeRuntimeState(): Promise<import("../ts/interface/IResponse").default>;
+}
