f75b8c13ffe79685b9e5ee7d842791cd25fa7318
Tony Yuan <tony.yuan@coolkit.cn>
2023-02-15 11:49:23 +0800

    Update event-online node

--- src/event-online.js
+++ src/event-online.js
@@ -1,17 +1,26 @@
 const { eventBridge } = require('./utils/event');
+const { EVENT_SSE_ON_UPDATE_DEVICE_ONLINE } = require('./utils/const');
+
 module.exports = function (RED) {
-    function GetStatusNode(config) {
+    function EventOnlineNode(config) {
         RED.nodes.createNode(this, config);
+        const node = this;
 
-        this.on('input',(msg) => {
-            msg.payload='test';
-            this.send(msg);
-        });
+        function eventSseOnUpdateDeviceOnlineHandler(jsonData) {
+            const data = JSON.parse(jsonData);
+            if (config.server === data.srcNodeId) {
+                // TODO: compare msg device and config.device
+                node.send({ payload: data.msg.data });
+            }
+        }
 
-        eventBridge.on('sse-state', (msg) => {
-           this.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', msg);
+        eventBridge.on(EVENT_SSE_ON_UPDATE_DEVICE_ONLINE, eventSseOnUpdateDeviceOnlineHandler);
+
+        node.on('close', (done) => {
+            eventBridge.off(EVENT_SSE_ON_UPDATE_DEVICE_ONLINE, eventSseOnUpdateDeviceOnlineHandler);
+            done();
         });
     }
 
-    RED.nodes.registerType('event-online', GetStatusNode);
+    RED.nodes.registerType('event-online', EventOnlineNode);
 };
