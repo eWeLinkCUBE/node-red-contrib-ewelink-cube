e7a724cb3f7e33be7c53e1ddb0028fe4f9d92a76
liyeping <1498328576@qq.com>
2023-02-16 14:32:11 +0800

    Get-device and Put-state-device

--- src/put-device-state.js
+++ src/put-device-state.js
@@ -1,10 +1,31 @@
+const ApiClient = require('./extern/libapi').default.ihostApi;
+const axios = require('axios');
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+const { API_URL_CONTROL_DEVICE } = require('./utils/const');
 module.exports = function (RED) {
     function PutDeviceStateNode(config) {
         RED.nodes.createNode(this, config);
-
-        this.on('input', (msg) => {
-            msg.payload='test';
-            this.send(msg);
+        let that = this;
+        let message = '';
+        this.on('input', async (msg) => {
+            this.log('config————————————————————————————————————————————》' + JSON.stringify(config));
+            const baseUrl = 'http://localhost:1880';
+            const url = baseUrl + API_URL_CONTROL_DEVICE;
+            let params = {
+                id: config.server,
+                deviceId: config.device,
+                params: config.state,
+            };
+            await axios
+                .post(url, params)
+                .then((res) => {
+                    message = 'success';
+                })
+                .catch((error) => {
+                    message = 'error';
+                });
+            msg.payload = message;
+            that.send(msg);
         });
     }
 
